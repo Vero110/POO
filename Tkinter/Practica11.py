0109f4f921add51fab556acfdb433d64a6ceaa0e
Vero110 <veronicaalmaraz59@gmail.com>
2023-03-01 18:03:36 -0600

    funciones para botones

--- Tkinter/Practica11.py
+++ Tkinter/Practica11.py
@@ -1,4 +1,22 @@
-from tkinter import Tk, Frame, Button 
+from tkinter import Tk, Frame, Button, messagebox
+
+#4. Funcion de mensajes para el botón
+def mostrarMensaje():
+    messagebox.showinfo("Aviso","Este mensaje es para avisar algo")
+    messagebox.showerror("Es un mensaje de error:","todo fallo con exito")
+    print (messagebox.askquestion("Pregunta:", "El jugó con tu corazón?"))
+    print (messagebox.askokcancel("Pregunta:", "El jugó con tu corazón?"))
+    print (messagebox.askretrycancel("Pregunta:", "El jugó con tu corazón?"))
+    print (messagebox.askyesnocancel("Pregunta:", "El jugó con tu corazón?"))
+    print (messagebox.askyesno("Pregunta:", "El jugó con tu corazón?"))
+
+#5. Funcion para agregar botones
+def agregarBoton():
+    botonRosa.config(text="+", bg = "white", fg = "pink")
+    botonNuevo = Button(seccion3, text ="Boton nuevo")
+    botonNuevo.pack()
+
+    
 
 #1. Crear ventana. Instanciamos un objeto Ventana
 ventana = Tk() 
@@ -16,7 +34,7 @@ seccion3 = Frame (ventana, bg = "gray")
 seccion3.pack(expand=True, fill = 'both')
 
 #3. Botones
-botonAzul = Button(seccion1, text = "boton azul", fg = "red") 
+botonAzul = Button(seccion1, text = "boton azul", fg = "red", command = mostrarMensaje) 
 botonAzul.place(x = 60, y = 60)
 
 botonMorado = Button(seccion2, text = "boton morado", fg = "#0099ff") 
@@ -25,8 +43,10 @@ botonMorado.grid(row = 0, column = 0 )
 botonNegro = Button(seccion2, text = "boton negro", fg = "black", bg = "#239023") 
 botonNegro.grid(row = 1, column = 0)
 
-botonRosa = Button(seccion3, text = "boton rosa", fg = "#00e600")
+botonRosa = Button(seccion3, text = "boton rosa", fg = "#00e600", command = agregarBoton)
 botonRosa.pack()
+
+
 
 #Main de ejecucion de la ventana 
 ventana.mainloop()
