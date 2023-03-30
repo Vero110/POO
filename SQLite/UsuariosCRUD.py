b4c3c7fea6abbd60cc12e4b2d512b4399059db58
Vero110 <veronicaalmaraz59@gmail.com>
2023-03-30 11:00:58 -0600

    Practica16Terminada

--- SQLite/UsuariosCRUD.py
+++ SQLite/UsuariosCRUD.py
@@ -18,7 +18,8 @@ def ejecutaSelectU():
     for usu in usuario: 
         cadena = str (usu [0]) + " " + usu[1] + " " + usu[2] + " " + str(usu[3])
     if (usuario ): 
-        print(cadena)
+        #print(cadena)
+        textEnc.insert("0.0", cadena)
     else:
         messagebox.showinfo("Usuario no encontroado","Ya no existe")
 
@@ -62,7 +63,8 @@ txtid =Entry (pestaña2, textvariable = varBus).pack()
 btnBus= Button(pestaña2, text = "Buscar", command= ejecutaSelectU).pack()
 
 subBus = Label (pestaña2, text = "Encontrado: ", fg = 'blue', font = ("Modern", 18)).pack()
-textEnc = tk.Text(pestaña2, height = 2, width = 52).pack()
+textEnc = tk.Text(pestaña2, height = 2, width = 52)
+textEnc.pack()
 
 
 panel.add(pestaña1, text='Formulario de usuarios')
