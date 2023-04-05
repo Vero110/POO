f2e721be69c88e12d15d8e84b32dcd38009e8707
Vero110 <veronicaalmaraz59@gmail.com>
2023-04-05 10:02:10 -0600

    Practica No.18

--- SQLite/UsuariosCRUD.py
+++ SQLite/UsuariosCRUD.py
@@ -1,8 +1,9 @@
+from logging import root
 from tkinter import *
 from tkinter import ttk
-from controladorBD import controladorBD
 import tkinter as tk
 from tkinter import messagebox
+from controladorBD import controladorBD
 
 # Creamos un objeto de la clase controladorBD
 controlador = controladorBD()
@@ -10,36 +11,56 @@ controlador = controladorBD()
 # Función para disparar el botón
 def ejecutaInsert():
     controlador.guardarUsuario(varNom.get(),varCor.get(),varCon.get())
-# Funcion para disparar el botón de busquedad
-def ejecutaSelectU():
-    usuario  = controlador.consultarUsuario(varBus.get())
-    
-    for usu in usuario:
-        cadena = str (usu[0]) + " " + usu [1] + " " + usu[2] + " " + str(usu[3])
-    if (usuario):
-    #print (cadena)
-        textEnc.insert("0.0", cadena)      
-    else:
-        
-        messagebox.showinfo("Usuario no encontrado","Ya no existe")
-        
-def ejecutaConsultaU():
-    usuarios = controlador.consultaUsuarios()
-    
-    #Vista de arbol
-    VUsuarios = ttk.Treeview(pestaña3, columns= ("Nombre", "Correo", "Contraseña"), show='headings')
-    VUsuarios.column("Nombre", width=50, minwidth=50, anchor=CENTER)
-    VUsuarios.column("Contraseña", width=50, minwidth=50, anchor=CENTER)
-    
-    VUsuarios.heading("Nombre", text="Nombre")    
-    VUsuarios.heading("Correo", text="Correo")    
-    VUsuarios.heading("Contraseña", text="Contraseña")   
-    
-    for usu in usuarios: 
-        VUsuarios.insert("", END, values = (usu[1], usu[2], usu[3]))
-        
-    VUsuarios.pack(fill = BOTH, expand=True)
 
+# Función para disparar el boton de busqueda
+def ejecutaSelectU():
+   usuario = controlador.consultarUsuario(varBus.get())
+   for usu in usuario:
+    
+    cadena = str(usu[0])+ " " + usu[1]+ " " + usu[2]+ " " + str(usu[3])
+
+   if(usuario):
+       textEnc.insert("0.0",cadena)
+   else:
+       messagebox.showinfo("Usuario no encontrado","Ya no existe")
+
+
+def cargarUsuarios():
+    # 1. Obtener los datos de la tabla
+    usuarios = controlador.consultarUsuarios()
+    #Vista de arbol
+    VUsuarios = ttk.Treeview(pestaña3, columns=("nombre", "correo", "contraseña"), show='headings')
+    VUsuarios.column("nombre", width=50, minwidth=50, anchor=CENTER)
+    VUsuarios.column("correo", width=50, minwidth=50, anchor=CENTER)
+    VUsuarios.column("contraseña", width=50, minwidth=50, anchor=CENTER)
+
+    VUsuarios.heading("nombre", text="Nombre")
+    VUsuarios.heading("correo", text="Correo")
+    VUsuarios.heading("contraseña", text="Contraseña")
+
+    for usuario in usuarios:
+        VUsuarios.insert("", END, values=(usuario[1], usuario[2], usuario[3]))
+
+    VUsuarios.pack(fill=BOTH, expand=True)
+
+def ejecutaActualizar():
+    idUsu = varIDa.get()
+    nomUsu = varNomac.get()
+    corrUsu = varCorac.get()
+    contUsu = varConta.get()
+    menConf = messagebox.askyesno(" ", "Desea actualizar los datos?")
+    if menConf:
+        controlador.actualizarUsuario(idUsu, nomUsu, corrUsu, contUsu)
+    else:
+        messagebox.showinfo("Error", "Los datos no se han actualizado")
+
+def ejecutaEliminar():
+    IDUsuario = varIDe.get()
+    menConf = messagebox.askyesno(" ", "Desea eliminar los datos?")
+    if menConf:
+        controlador.eliminarUsuario(IDUsuario)
+    else:
+        messagebox.showinfo(" ", "Los datos no han sido eliminados")
         
 ventana = Tk()
 ventana.title("CRUD de Usuarios")
@@ -52,42 +73,83 @@ pestaña1 = ttk.Frame(panel)
 pestaña2 = ttk.Frame(panel)
 pestaña3 = ttk.Frame(panel)
 pestaña4 = ttk.Frame(panel)
+pestaña5 = ttk.Frame(panel)
 
 # Pestaña1: Formulario de usuarios
+
 Label(pestaña1, text='Registro de usuarios', fg='blue', font=('Modern', 18)).pack()
+
 varNom = StringVar()
 Label(pestaña1, text="Nombre: ").pack()
 Entry(pestaña1, textvariable=varNom).pack()
+
 varCor = StringVar()
 Label(pestaña1, text="Correo: ").pack()
 Entry(pestaña1, textvariable=varCor).pack()
+
 varCon = StringVar()
 Label(pestaña1, text="Contraseña: ").pack()
-Entry(pestaña1, textvariable=varCon).pack()
+Entry(pestaña1, textvariable=varCon, show="*").pack()
+
 btnGuardar = Button(pestaña1, text="Guardar Usuario", command=ejecutaInsert)
 btnGuardar.pack()
 
+#Pestaña 2: Buscar Usuarios
 
-#Pestaña2: Buscar usuario 
-titulo2 = Label(pestaña2, text = "Buscar Usuario", fg = 'green', font = ("Modern", 18)).pack()
+titulo2 = Label(pestaña2,text="Buscar Usuario", fg='green', font=("Modern",18)).pack()
 
 varBus = tk.StringVar()
-lblid = Label (pestaña2, text = " Identificador Usuario:").pack()
-txtid =Entry (pestaña2, textvariable = varBus).pack()
-btnBus= Button(pestaña2, text = "Buscar", command= ejecutaSelectU).pack()
+lblid = Label(pestaña2,text= "Identificador de usuarios: ").pack()
+txtid = Entry(pestaña2,textvariable=varBus).pack()
+btnBus = Button(pestaña2, text="Buscar",command=ejecutaSelectU).pack()
 
-subBus = Label (pestaña2, text = "Encontrado: ", fg = 'blue', font = ("Modern", 18)).pack()
-textEnc = tk.Text(pestaña2, height = 2, width = 52)
+subBus = Label(pestaña2,text="Encontrado: ", fg='green', font=("Modern",15)).pack()
+textEnc = tk.Text(pestaña2,height=5,width=52)
 textEnc.pack()
 
-#Pestaña3: Consultar usuario 
-titulo3 = Label(pestaña3, text = "Consultar Usuarios", fg = 'green', font = ("Modern", 18)).pack()
+#Pestaña 3: Consultar usuarios
 
-btnCons = Button(pestaña3, text="Mostrar todos los usuarios: ", command=ejecutaConsultaU).pack()
+titulo3 = Label(pestaña3, text="Consultar Usuarios", fg='green', font=("Modern", 18)).pack()
+btnCargarUsuarios = Button(pestaña3, text="Mostrar todos los usuarios:", command=cargarUsuarios).pack()
+
+#Pestaña 4: Actualizar Usuario
+
+titulo4 = Label(pestaña4,text="Actualizar usuario", fg='green', font=("Modern",18)).pack()
+
+titulo5 = Label(pestaña4,text="ID del usuario a actualizar", fg='green', font=("Modern",10)).pack()
+varIDa = StringVar()
+Label(pestaña4, text="ID  ").pack()
+Entry(pestaña4, textvariable=varIDa).pack()
+
+titulo6 = Label(pestaña4,text="Datos del usuario a actualizar", fg='green', font=("Modern",10)).pack()
+varNomac = StringVar()
+Label(pestaña4, text="Nombre").pack()
+Entry(pestaña4, textvariable=varNomac).pack()
+
+varCorac = StringVar()
+Label(pestaña4, text="Correo").pack()
+Entry(pestaña4, textvariable=varCorac).pack()
+
+varConta = StringVar()
+Label(pestaña4, text="Contraseña").pack()
+Entry(pestaña4, textvariable=varConta, show="*").pack()
+
+btnActualizar = Button(pestaña4, text="Actualizar", command=ejecutaActualizar)
+btnActualizar.pack()
+
+#Pestaña 5: Eliminar Usuario
+titulo5 = Label(pestaña5,text="Eliminar Usuario", fg='green', font=("Modern",18)).pack()
+varIDe = StringVar()
+Label(pestaña5, text="ID: ").pack()
+Entry(pestaña5, textvariable=varIDe).pack()
+
+btnEliminar = Button(pestaña5, text="Eliminar", command=ejecutaEliminar)
+btnEliminar.pack()
 
 panel.add(pestaña1, text='Formulario de usuarios')
 panel.add(pestaña2, text='Buscar usuario')
 panel.add(pestaña3, text='Consultar usuarios')
 panel.add(pestaña4, text='Actualizar usuario')
+panel.add(pestaña5, text='Eliminar usuario')
 
 ventana.mainloop()
--- SQLite/controladorBD.py
+++ SQLite/controladorBD.py
@@ -1,95 +1,145 @@
 from tkinter import messagebox
-import sqlite3 
-import bcrypt 
+import sqlite3
+import tkinter
+from typing import Self
+import bcrypt
 
 class controladorBD:
-    def __init__(self):
-        pass 
-    
-    # Metodo para la creacion de conexiones 
+
+    def _init_(self):
+        pass
+
+    # Metodo para crear conexiones
     def conexionBD(self):
-        
+
         try:
             conexion = sqlite3.connect("C:/Users/52442/Documents/GitHub/POO/SQLite/DBUsuarios.db")
-            print("Conectado a la base de datos")
-            
-            return conexion 
-
+            print("Conectado a la Base de Datos")
+            return conexion
         except sqlite3.OperationalError:
-            print("No se conecto a la base de datos")
-        
-    #Metodo para guardar usuarios en la BD
-    def guardarUsuario(self, nom, cor, con):
-        
-        #1. Importar una conexion 
-        #Usamos una conexion 
+            print("No se puede conectar")
+
+    #Metodo para guardar usuarios
+    def guardarUsuario(self,nom,cor,con):
+
+        # usamos una conexion
         conx = self.conexionBD()
-        
-        #2. Validar parametros VACIOS
-        #no dejamos avanzar para no meter cosas que no queremos en la BD
-        if (nom == "" or cor == "" or con == ""): 
-            messagebox.showwarning("Advertencia", "El Formulario esta Incompleto")
-        else: 
-            #3. Preparar el CURSOR, DATOS, QUERYSQL
+
+        # validar parametros que esten vacios
+        if(nom=="" or cor=="" or con ==""):
+            messagebox.showwarning("Formulario Incompleto")
+        else:
+        #preparamos el cursor Y EL QUERY
             cursor = conx.cursor()
-            conH = self.encriptarCon(con) #la contraseña encriptada guardamela en conH
-            datos = (nom, cor, conH) 
-            qrInsert = "insert into TBRegistrados(Nombre, Correo, Contraseña) values(?,?,?)"
-            
-            #4. Ejecutar insert y cerramos conexion 
-            cursor.execute(qrInsert, datos)
-            conx.commit() #Cambio en la base de datos
+            conH = self.encriptarCon(con)
+            datos = (nom,cor,conH)
+            qrInsert = "insert into TBRegistrados(nombre,correo,contraseña) values(?,?,?)"
+
+        #ejecutar Insert y cerramos conexion
+            cursor.execute(qrInsert,datos)
+            conx.commit()
             conx.close
-            messagebox.showinfo("Listo", "Usuario guardado")
-    
-       #Metodo para encriptar contraseñas
-    def encriptarCon(self, con): 
-        conPlana = con 
+            messagebox.showinfo("Exito","Usuario Guardado")
+
+    def encriptarCon(self,con):
+        conPlana = con
         conPlana = conPlana.encode() #convertimos con a bytes
-        sal = bcrypt.gensalt() #algoritmo de aleatoridad
-        conHa = bcrypt.hashpw(conPlana, sal) #crea la contraseña encriptada
-        print (conHa)
-        #enviamos la contraseña encryptada
-        return conHa      
-    
-        #Metodo para buscar 1 usuario 
-    def consultarUsuario(self, id):
-        #1. Preparar una conexion 
+        sal = bcrypt.gensalt()
+        conHa = bcrypt.hashpw(conPlana,sal)
+        print(conHa)
+
+        return conHa
+
+     # Metodo para buscar un usuario
+    def consultarUsuario(self,id):
+        #1. Preparar una conexion
         conx = self.conexionBD()
-        #2. verificar si id contiene algo 
-        if (id == ""): 
-            messagebox.showwarning("Cuidado", "Id vacio, escribe algo valido")
+
+        if(id == ""):
+            messagebox.showwarning("Advertencia","El ID esta vacío")
             conx.close()
         else:
             try:
-                #3. preparar lo necesario (CURSOR Y EL QUERY)
-                cursor=conx.cursor()
-                selectQry = "select * from TBRegistrados where id = " + id
-                
-                #4. ejecutar y guardar la consulta
-                cursor.execute(selectQry)
+    
+                cursor = conx.cursor()
+                selecQuery = "select * from TBRegistrados where id="+id
+
+                cursor.execute(selecQuery)
                 rsUsuario = cursor.fetchall()
                 conx.close()
-                
+
                 return rsUsuario
-            
+
             except sqlite3.OperationalError:
                 print("Error de consulta")
-    
-    def consultaUsuarios(self):
-        #1. Preparar una conexion 
+
+
+    def consultarUsuarios(self):
+    # 1. usamos conexion 
         conx = self.conexionBD()
-        
-        #2. preparamos el CURSOR y el QUERY
         cursor = conx.cursor()
+
         consultaQry = "SELECT * FROM TBRegistrados"
-        
-        # ejecutar y guardar la consulta 
         cursor.execute(consultaQry)
-        rsUsuarios = cursor.fetchall()
+        rsConsulta = cursor.fetchall()
+
+    # cerrar Conexion
         conx.close()
-        
-        return rsUsuarios
-        
-            
-        
+
+        return rsConsulta
+
+        # Método para actualizar un usuario
+    def actualizarUsuario(self, id, nom, cor, con):
+        # 1. Preparar una conexión
+        conx = self.conexionBD()
+
+        # 2. Verificar si el ID está vacío
+        if(id == ""):
+            messagebox.showwarning(" ","El ID esta vacio")
+            conx.close()
+        else:
+            try:
+                # 3. Preparar cursor y query
+                cursor = conx.cursor()
+
+                # 4. Encriptar la contraseña si es necesario
+                if con != "":
+                    con = self.encriptarCon(con)
+
+                # 5. Actualizar los datos del usuario
+                actualizaQry = f"UPDATE TBRegistrados SET nombre=?, correo=?, contraseña=? WHERE id={id}"
+                Obj = (nom, cor, con)
+                cursor.execute(actualizaQry, Obj)
+
+                # 6. Guardar los cambios y cerrar la conexión
+                conx.commit()
+                conx.close()
+
+                messagebox.showinfo(" ", "El usuario se ha actualizado")
+            except sqlite3.OperationalError:
+                print("No se puede actualizar el usuario")
+
+    def eliminarUsuario(self, id):
+            # 1. Preparar una conexión
+            cons = self.conexionBD()
+
+            # 2. Verificar si el ID está vacío
+            if(id == ""):
+                messagebox.showwarning(" ","No podemos seguir porque el ID esta vacio")
+                cons.close()
+            else:
+                try:
+                    # 3. Preparar cursor y query
+                    cursor = cons.cursor()
+
+                    # 4. Eliminar el usuario
+                    delQuery = f"DELETE FROM TBRegistrados WHERE id={id}"
+                    cursor.execute(delQuery)
+
+                    # 5. Guardar los cambios y cerrar la conexión
+                    cons.commit()
+                    cons.close()
+
+                    messagebox.showinfo("" , "El usuario ha sido eliminado exitosamente")
+                except sqlite3.OperationalError:
+                    print("No se puede eliminar el usuario")
